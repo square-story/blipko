7b961f3578c9864b6aa5e751a5beb421d527fe21
SADIK <gibmepreo@gmail.com>
2025-12-01 11:54:13 +0530

    feat: type annotate app and routes; guard category update

--- src/presentation/routes/webhookRoutes.ts
+++ src/presentation/routes/webhookRoutes.ts
@@ -1,15 +1,13 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { webhookController } from '../controllers/WebhookController';
+import { webhookController } from "../controllers/WebhookController";
 
-export const webhookRoutes = Router();
+export const webhookRoutes: Router = Router();
 
-webhookRoutes.get('/', (req, res, next) =>
+webhookRoutes.get("/", (req, res, next) =>
   webhookController.verifyWebhook(req, res, next),
 );
 
-webhookRoutes.post('/', (req, res, next) =>
+webhookRoutes.post("/", (req, res, next) =>
   webhookController.handleWebhook(req, res, next),
 );
-
-
