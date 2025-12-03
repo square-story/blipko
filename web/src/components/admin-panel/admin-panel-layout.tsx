93f17589067ff11c4694b4151eb4db8ad3cd2aa8
SADIK <gibmepreo@gmail.com>
2025-12-03 20:36:06 +0530

    feat: Add `data-vaul-drawer-wrapper` attribute to the admin panel layout.

--- web/src/components/admin-panel/admin-panel-layout.tsx
+++ web/src/components/admin-panel/admin-panel-layout.tsx
@@ -36,6 +36,7 @@ export default function AdminPanelLayout({
           "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
           !settings.disabled && (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-72")
         )}
+        data-vaul-drawer-wrapper=""
       >
         {children}
       </main>
