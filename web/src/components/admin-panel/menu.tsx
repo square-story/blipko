557df85c2ecd4b7ed8108ddbaeaca38d2c293697
SADIK <gibmepreo@gmail.com>
2025-12-03 17:49:42 +0530

    refactor: remove pathname parameter from getMenuList call in admin panel menu

--- web/src/components/admin-panel/menu.tsx
+++ web/src/components/admin-panel/menu.tsx
@@ -23,7 +23,7 @@ interface MenuProps {
 
 export function Menu({ isOpen }: MenuProps) {
   const pathname = usePathname();
-  const menuList = getMenuList(pathname);
+  const menuList = getMenuList();
 
   return (
     <ScrollArea className="[&>div>div[style]]:!block">
